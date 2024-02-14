3ef5fd0f3e2f0d4cc4f4d052682e9755b36497ca
almatkai <kairatovalmat2003@gmail.com>
2024-02-14 12:48:21 +0600

    Initial Commit

---
+++ DIPLOMA SWIFTUI/DIPLOMA_SWIFTUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIPLOMA_SWIFTUIApp.swift
+//  DIPLOMA SWIFTUI
+//
+//  Created by Almat Kairatov on 14.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct DIPLOMA_SWIFTUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
